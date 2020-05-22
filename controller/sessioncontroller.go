605f5ffaa850f971a18fa101995276508f8af3cf
Nils Hasenbanck <nils@hasenbanck.de>
2020-05-22 14:36:49 +0200

    Add legacy path for the GET handler.

--- controller/sessioncontroller.go
+++ controller/sessioncontroller.go
@@ -56,6 +56,7 @@ func (c *SessionController) RegisterRoutes(server *echo.Echo) {
 	server.GET("/list/", c.List) // Legacy path
 	server.GET("/", c.Index)
 	server.GET("/:roomID", c.Get)
+	server.GET("/:roomID/", c.Get) // Legacy path
 }
 
 // PrerenderTemplates prerenders all templates
